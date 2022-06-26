cf3eea7296f97a068952be71a5208465eff9d4fb
Blagoj <blagoj.petrov5@gmail.com>
2022-06-26 20:03:30 +0200

    Improve scripts

--- init.sh
+++ init.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
-# Moving logic
-# Moving tmux
+echo "Moving bin to ~/.local/bin"
+if [ ! -d ~/.local/bin ]; then 
+  mkdir -p ~/.local/bin
+fi
+cp -r ./.local/bin/* ~/.local/bin/
+
+echo "Moving personal to ~/.config/personal"
+if [ ! -d ~/.config/personal ]
+then 
+  mkdir -p ~/.config/personal
+fi
+cp -r ./.config/personal/* ~/.config/personal/
 
 echo "Moving zsh to root"
 if [ -f ~/.zshrc ]; then 
-  echo "Moving old .zshrc to .zshrc.backup"
-  mv ~/.zshrc ~/.zshrc.backup
+  echo -n "~/.zshrc already exists, overwrite? (y/n)"
+  read
+  if [ "$REPLY" = "n" ]; then
+    echo "Existing installation"
+  elif [ "$REPLY" = "y" ]
+    rm ~/.zshrc
+  fi
 fi
 cp ./.zshrc ~/.zshrc
+
 if [ -f ~/.zsh_profile ]; then 
   echo "Moving old .zsh_profile to .zsh_profile.backup"
   mv ~/.zsh_profile ~/.zsh_profile.backup
@@ -27,39 +43,25 @@ if [ -d ~/.config/i3 ]; then
   echo "Moving old i3 to i3.backup"
   mv ~/.config/i3 ~/.config/i3.backup
 fi
-cp -r ./.config/i3 ~/.config/i3
+cp -r ./.config/i3/* ~/.config/i3
 if [ -d ~/.config/i3status ]; then 
   echo "Moving old i3status to i3status.backup"
   mv ~/.config/i3status ~/.config/i3status.backup
 fi
-cp -r ./.config/i3status ~/.config/i3status
-
-echo "Moving personal to ~/.config/personal"
-if [ -d ~/.config/personal ]
-then 
-  cp -r ./.config/personal/* ~/.config/personal/*
-else 
-  cp -r ./.config/personal ~/.config/personal
-fi
+cp -r ./.config/i3status/* ~/.config/i3status
 
 echo "Moving alacritty to ~/.config/alacritty"
 if [ -d ~/.config/alacritty ]; then 
   echo "Moving old alacritty to alacritty.backup"
   mv ~/.config/alacritty ~/.config/alacritty.backup
 fi
-cp -r ./.config/alacritty ~/.config/alacritty
+cp -r ./.config/alacritty/* ~/.config/alacritty
 
 echo "Moving fonts to ~/.local/share/fonts"
 if [ ! -d ~/.local/share/fonts ]; then 
   mkdir -p ~/.local/share/fonts
 fi
 cp -r ./.local/share/fonts/* ~/.local/share/fonts/
-
-echo "Moving bin to ~/.local/bin"
-if [ ! -d ~/.local/bin ]; then 
-  mkdir -p ~/.local/bin
-fi
-cp -r ./.local/bin/* ~/.local/bin/
 
 echo "Installing fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
---
+++ installation.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# sudo apt update
+
+i3=`command -v i3`
+if [ -z "$i3" ]; then
+  sudo apt install i3
+fi
+
+zsh=`command -v zsh`
+if [ -z "$zsh" ]; then
+  sudo apt install zsh
+  chsh -s $(which zsh)
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+alacritty=`command -v alacritty`
+if [ -z "$alacritty" ]; then
+  sudo add-apt-repository ppa:aslatter/ppa
+  sudo apt update
+  sudo apt install alacritty
+fi
+
+tmux=`command -v tmux`
+if [ -z "$tmux" ]; then
+  sudo apt-get update
+  sudo apt-get install tmux
+fi
+
+curl=`command -v curl`
+if [ -z "$curl" ]; then
+  sudo apt install curl
+fi
+
+nvim=`command -v nvim`
+if [ -z "$nvim" ]; then
+  curl -o ./nvim-linux64.deb -JLO https://github.com/neovim/neovim/releases/download/v0.7.0/nvim-linux64.deb
+  sudo apt install ./nvim-linux64.deb
+  rm ./nvim-linux64.deb
+fi
