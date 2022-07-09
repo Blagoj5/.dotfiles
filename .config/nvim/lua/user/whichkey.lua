a20680e500b7ff96227173e50745421aa4c10d9e
Baze <blagoj.petrov5@gmail.com>
2022-07-09 22:02:57 +0200

    Update which key lsp diagnostic

--- .config/nvim/lua/user/whichkey.lua
+++ .config/nvim/lua/user/whichkey.lua
@@ -160,7 +160,7 @@ local mappings = {
       "Prev Diagnostic",
     },
     l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
-    q = { "<cmd>lua vim.lsp.diagnostic.setloclist()<cr>", "Quickfix" },
+    q = { "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", "Quickfix" },
     r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
     s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
     S = {
