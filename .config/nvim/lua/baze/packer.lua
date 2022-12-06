9579aefefc16177a3155f5962df99c052ec62b4d
Blagoj Petrov <blagoj.petrov5@gmail.com>
2022-12-07 00:04:41 +0100

    Update packer

--- .config/nvim/lua/baze/packer.lua
+++ .config/nvim/lua/baze/packer.lua
@@ -68,6 +68,11 @@ return packer.startup(function(use)
 	use("hrsh7th/cmp-buffer") -- source for text in buffer
 	use("hrsh7th/cmp-path") -- source for file system paths
 
+  -- snippets
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
 	-- managing & installing lsp servers, linters & formatters
 	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
 	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
